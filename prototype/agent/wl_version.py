02922ed5f90db45dd55cd58da354e1220fa91649
Alkalid-security <awais.envison@gmail.com>
2026-09-12 19:41:45 +0500

    chore(release): bump final Agent to 0.4.4

--- prototype/agent/wl_version.py
+++ prototype/agent/wl_version.py
@@ -13,7 +13,7 @@ from __future__ import annotations
 
 import os
 
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 BUILD_SHA = os.environ.get("WATCHLOG_BUILD_SHA", "")
 BUILD_CHANNEL = os.environ.get("WATCHLOG_BUILD_CHANNEL", "production")
 
